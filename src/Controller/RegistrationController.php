9a8e4284f32659b5610a3df039fd2e5de213882c
perdreauarthur <arthur.perdreau@outlook.fr>
2025-06-08 14:42:57 +0200

    user system

---
+++ src/Controller/RegistrationController.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\RegistrationForm;
+use App\Repository\UserRepository;
+use App\Security\EmailVerifier;
+use App\Service\JWTService;
+use App\Service\SendEmailService;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Security\SecurityAuthenticator;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mime\Address;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Authentication\UserAuthenticatorInterface;
+use Symfony\Contracts\Translation\TranslatorInterface;
+use SymfonyCasts\Bundle\VerifyEmail\Exception\VerifyEmailExceptionInterface;
+
+class RegistrationController extends AbstractController
+{
+    #[Route('/register', name: 'app_register')]
+    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, UserAuthenticatorInterface $userAuthenticator, SecurityAuthenticator $authenticator, EntityManagerInterface $entityManager, JWTService $jwt, SendEmailService $mail): Response
+    {
+        $user = new User();
+        $form = $this->createForm(RegistrationForm::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // encode the plain password
+            $user->setPassword(
+                $userPasswordHasher->hashPassword(
+                    $user,
+                    $form->get('plainPassword')->getData()
+                )
+            );
+
+
+
+            $entityManager->persist($user);
+            $entityManager->flush();
+            // do anything else you need here, like send an email
+
+            // Générer le token
+            // Header
+            $header = [
+                'typ' => 'JWT',
+                'alg' => 'HS256'
+            ];
+
+            // Payload
+            $payload = [
+                'user_id' => $user->getId()
+            ];
+
+            // On génère le token
+            $token = $jwt->generate($header, $payload, $this->getParameter('app.jwtsecret'));
+
+            // Envoyer l'e-mail
+            $mail->send(
+                'contact@arthurperdreau.com',
+                $user->getEmail(),
+                'Account Verification ',
+                'register',
+                compact('user', 'token') // ['user' => $user, 'token'=>$token]
+            );
+
+
+            $this->addFlash('success', 'Utilisateur inscrit, veuillez cliquer sur le lien reçu pour confirmer votre adresse e-mail');
+
+            return $this->redirectToRoute('app_login');
+        }
+
+        return $this->render('registration/register.html.twig', [
+            'registrationForm' => $form
+        ]);
+    }
+
+    #[Route('/verif/{token}', name: 'verify_user')]
+    public function verifUser($token, JWTService $jwt, UserRepository $usersRepository, EntityManagerInterface $em): Response
+    {
+        // On vérifie si le token est valide (cohérent, pas expiré et signature correcte)
+        if($jwt->isValid($token) && !$jwt->isExpired($token) && $jwt->check($token, $this->getParameter('app.jwtsecret'))){
+            // Le token est valide
+            // On récupère les données (payload)
+            $payload = $jwt->getPayload($token);
+
+            // On récupère le user
+            $user = $usersRepository->find($payload['user_id']);
+
+            // On vérifie qu'on a bien un user et qu'il n'est pas déjà activé
+            if($user && !$user->isVerified()){
+                $user->setIsVerified(true);
+                $em->flush();
+
+                $this->addFlash('success', 'Utilisateur activé');
+                return $this->redirectToRoute('app_login');
+            }
+        }
+        $this->addFlash('danger', 'Le token est invalide ou a expiré');
+        return $this->redirectToRoute('app_login');
+    }
+}
